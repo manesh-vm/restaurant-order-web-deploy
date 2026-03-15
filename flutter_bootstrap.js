3541fc6c81ab7dd49ea755dccc15a245bc1df107
manesh-vm <42232482+manesh-vm@users.noreply.github.com>
2026-03-15 19:11:45 +0000

    Deploying to gh-pages from @ manesh-vm/restaurant@56c48a1096372b5b23b843269711d5dc3ab1ebc8 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2504576454" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "839635587" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
